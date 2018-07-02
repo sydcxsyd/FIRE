d4e67a34c9d94254819e3172a4b55384a6c7154b
su yingdi <516300464@qq.com>
2018-07-02 17:39:07 +0800

    Initial commit

---
+++ assets/Script/Scene/FightScene.js
@@ -0,0 +1,19 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        label: {
+            default: null,
+            type: cc.Label
+        },
+    },
+        
+    onLoad: function () {
+        
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
